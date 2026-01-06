c4658462942f0d593481312caa307058779fa0a3
Prashant <prashantco111@gmail.com>
2026-01-06 10:27:03 +0530

    Changed names

---
+++ target/debug/deps/add-4a9ac0c1ec07de8a.d
@@ -0,0 +1,7 @@
+/Users/prashantsingh/Desktop/WWC/Learn-Rust/git_tag/target/debug/deps/add-4a9ac0c1ec07de8a.d: src/lib.rs
+
+/Users/prashantsingh/Desktop/WWC/Learn-Rust/git_tag/target/debug/deps/libadd-4a9ac0c1ec07de8a.rlib: src/lib.rs
+
+/Users/prashantsingh/Desktop/WWC/Learn-Rust/git_tag/target/debug/deps/libadd-4a9ac0c1ec07de8a.rmeta: src/lib.rs
+
+src/lib.rs:
